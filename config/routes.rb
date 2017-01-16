624c5fa5f0c59b6e12dda1234e25a943e0e114bf
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-01-16 18:00:03 +0900

    デバイスの画面をルートに設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   get 'home/show'
 
   root to: "home#index"
+
+  # ログイン画面をホームにする
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
