2a9c8656b0ba60b8a02dc806a293679066d19ef1
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-01-16 18:03:16 +0900

    URL(Route)のカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, path_names: { sign_in: "login", sign_out: "logout" }
   get 'home/index'
 
   get 'home/show'
